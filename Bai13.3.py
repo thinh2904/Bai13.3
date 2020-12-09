4b167bf66d9b2e4fd4778c4e83b45ca3675132ce
thinh2904 <72325160+thinh2904@users.noreply.github.com>
2020-12-09 09:54:38 +0700

    Update Bai13.3.py

--- Bai13.3.py
+++ Bai13.3.py
@@ -1,4 +1,3 @@
-import os
 import random
 import string
 size=int(input('Nhập giới hạn dữ liệu (MB): '))
@@ -7,13 +6,10 @@ while size<1 or size>1024:
 one_file=1000*1024
 nguyen=size*(2**20)//(one_file)
 du=size*(2**20)%(one_file)
-ky_tu=string.ascii_lowercase*39385
-def text():
-    a=''.join(random.sample(ky_tu,one_file))
-    return a
+ky_tu=string.ascii_lowercase*100000
 for i in range(nguyen):
 	file=open('File'+str(i),'w')
-	file.write(text())
+	file.write(a=''.join(ky_tu,one_file)) 
 	file.close()
 file_cuoi=open('File cuối','w')
 file_cuoi.write(''.join(random.sample(ky_tu,du)))
